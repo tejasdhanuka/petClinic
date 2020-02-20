d66f0ca075744846dac13fe2c129019cf19cb9fe
Tejas, Dhanuka <ts-dhanuka.tejas@rakuten.com>
2020-02-20 12:56:55 +0900

    Initial Project commit

---
+++ petClinic/petClinic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  petClinic
+//
+//  Created by Tejas, Dhanuka on 2020/02/20.
+//  Copyright © 2020 Tejas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
