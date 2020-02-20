0a9f9b60e939786b55a584b0937b06d9d52ee525
Tejas, Dhanuka <ts-dhanuka.tejas@rakuten.com>
2020-02-20 12:57:27 +0900

    Call chat button and timing view added

--- petClinic/petClinic/ContentView.swift
+++ petClinic/petClinic/ContentView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Chat")
+                        .font(.body)
+                        .padding()
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                })
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding(.horizontal, 10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Call")
+                        .font(.body)
+                        .padding()
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                })
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding(.horizontal, 10)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            
+            Text("Office Hours: M-F 10:00 - 18:00")
+                .frame(height: 44)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+                .padding(.horizontal, 10)
+        }
     }
 }
 
